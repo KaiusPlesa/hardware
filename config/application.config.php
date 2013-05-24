622541ded40aed059e327773ad252c069ff3eb05
KaiusPlesa <kaius_plesa@yahoo.com>
2013-05-24 20:50:51 +0300

    Zfcuser instal

--- config/application.config.php
+++ config/application.config.php
@@ -5,6 +5,8 @@ return array(
         'Application',
         'Album',
         'Users',
+        'ZfcBase',
+        'ZfcUser',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
