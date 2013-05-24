3ed1c978435c6dafc7aea019b2b13c5330c956a7
KaiusPlesa <kaius_plesa@yahoo.com>
2013-05-24 22:07:51 +0300

    ZFUser modif and tests

--- config/application.config.php
+++ config/application.config.php
@@ -7,6 +7,7 @@ return array(
         'Users',
         'ZfcBase',
         'ZfcUser',
+        'BjyAuthorize',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
